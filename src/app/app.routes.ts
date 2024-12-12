355dbfdffc87fbaef2744109a26d60b07f71c7e3
ncad24 <149948740+ncad24@users.noreply.github.com>
2024-12-12 15:09:26 -0500

    Create and configure the proyect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {LandingOneComponent} from './component/landing-one/landing-one.component';
+import {LandingTwoComponent} from './component/landing-two/landing-two.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component: LandingOneComponent},
+  {path:'/landingOne', component: LandingOneComponent},
+  {path:'/landingTwo', component: LandingTwoComponent},
+];
