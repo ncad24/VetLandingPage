73d582dcafa648570b0ee5e56f65afa837eb7f69
ncad24 <149948740+ncad24@users.noreply.github.com>
2024-12-18 23:43:21 -0500

    add the new sections of the first part of landing

--- src/app/component/landing-one/landing-one.component.ts
+++ src/app/component/landing-one/landing-one.component.ts
@@ -1,7 +1,11 @@
-import {Component, HostListener} from '@angular/core';
+import {Component, HostListener, inject} from '@angular/core';
 import {MatButton} from '@angular/material/button';
 import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 import {MatToolbar} from '@angular/material/toolbar';
+import {ViewportScroller} from '@angular/common';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {MatOption, MatSelect} from '@angular/material/select';
 
 @Component({
   selector: 'app-landing-one',
@@ -10,15 +14,19 @@ import {MatToolbar} from '@angular/material/toolbar';
     MatMenu,
     MatMenuItem,
     MatToolbar,
-    MatMenuTrigger
-
+    MatMenuTrigger,
+    MatFormField,
+    MatInput,
+    MatSelect,
+    MatOption,
+    MatLabel
   ],
   templateUrl: './landing-one.component.html',
   standalone: true,
   styleUrl: './landing-one.component.css'
 })
 export class LandingOneComponent {
-
+  private viewportScroller: ViewportScroller = inject(ViewportScroller);
   @HostListener('window:scroll', [])
   onWindowScroll(): void {
     const navbar = document.getElementById('navbar');
@@ -37,6 +45,10 @@ export class LandingOneComponent {
         }
       }
     }
+  }
+
+  scrollToSection(fragment: string): void {
+    this.viewportScroller.scrollToAnchor(fragment);
   }
 
 }
